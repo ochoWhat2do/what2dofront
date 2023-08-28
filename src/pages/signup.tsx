bcd0fd5cc17c35d0b3b9c1f5ab3095814cba27ad
sxi8022 <sxi8022@gmail.com>
2023-08-28 22:42:09 +0900

    이메일 유효성체크

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import axios from "axios";
 import Header from "./components/Header";
 import styles from "../styles/signup.module.css";
+import { useRouter } from "next/router";
 
 const Signup = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password_confirm, setPasswordConfirm] = useState("");
@@ -14,6 +16,27 @@ const Signup = () => {
   const [city, setCity] = useState(""); // State for city input
   const [gender, setGender] = useState(""); // State for gender select
   const [passwordMismatch, setPasswordMismatch] = useState(false);
+
+  const handleEmailBlur = async () => {
+    if (!validateEmail(email)) {
+      window.alert("유효한 이메일 형식이 아닙니다.");
+      return;
+    }
+
+    try {
+      debugger;
+      const response = await axios.get(
+        `http://localhost:8080/api/users/checkEmail?email=${email}`
+      );
+
+      if (response.data) {
+        window.alert("이미 등록된 이메일입니다.");
+        setEmail(""); // Clear email input
+      }
+    } catch (error) {
+      console.error("이메일 중복 체크 오류:", error);
+    }
+  };
 
   const handleSignup = async () => {
     // Check if password and password_confirm match
@@ -29,9 +52,17 @@ const Signup = () => {
       return;
     }
 
+    // password validation
+    if (!password || !validatePassword(password)) {
+      window.alert(
+        "숫자,소문자,특수문자를 각각 1자씩 포함한 8자 이상으로 비밀번호를 구성해야 합니다."
+      );
+      return;
+    }
+
     // Nickname and password validation
-    if (!nickname || !password) {
-      window.alert("닉네임과 비밀번호는 필수 입력 항목입니다.");
+    if (!nickname) {
+      window.alert("닉네임은 필수 입력 항목입니다.");
       return;
     }
 
@@ -57,6 +88,7 @@ const Signup = () => {
 
       if (response.status === 200) {
         setMessage("회원가입이 성공적으로 완료되었습니다.");
+        router.push("/login");
       }
     } catch (error) {
       setMessage("회원가입 중 오류가 발생했습니다.");
@@ -72,122 +104,127 @@ const Signup = () => {
     return emailRegex.test(email);
   };
 
+  const validatePassword = (password: string): boolean => {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    const isValid = passwordRegex.test(password);
+    return isValid;
+  };
+
   return (
     <div>
-      <Header />
-      <div>
-        <Header /> {/* Render the Header component here */}
-        <div className={styles["signup-form"]}>
-          <div className={styles["signup-title"]}>회원가입</div>
-          <div className={styles["signup-input-container"]}>
-            <div className="signup-id-label">이메일</div>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className={styles["signup-input-box"]}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className={styles["signup-input-container"]}>
-            <div className="signup-id-label">비밀번호</div>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className={styles["signup-input-box"]}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className={styles["signup-input-container"]}>
-            <div className="signup-id-label">비밀번호확인</div>
-            <input
-              type="password"
-              name="password_confirm"
-              id="password_confirm"
-              className={styles["signup-input-box"]}
-              value={password_confirm}
-              onBlur={() => {
-                if (password !== password_confirm) {
-                  setPasswordMismatch(true);
-                } else {
-                  setPasswordMismatch(false);
-                }
-              }}
-              onChange={(e) => {
-                setPasswordConfirm(e.target.value);
-              }}
-            />
-            {passwordMismatch && (
-              <p style={{ color: "red" }}>
-                비밀번호와 비밀번호 확인의 내용이 다릅니다.
-              </p>
-            )}
-          </div>
-          <div className={styles["signup-input-container"]}>
-            <div className="signup-id-label">닉네임</div>
-            <input
-              type="text"
-              name="nickname"
-              id="nickname"
-              className={styles["signup-input-box"]}
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)}
-            />
-          </div>
-          <div className={styles["signup-input-container"]}>
-            <div className="signup-id-label">도시</div>
-            <input
-              type="text"
-              name="city"
-              id="city"
-              className={styles["signup-input-box"]}
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </div>
-          <div className={styles["signup-input-container"]}>
-            <div className="signup-id-label">성별</div>
-            <select
-              name="gender"
-              id="gender"
-              className={styles["signup-input-box"]}
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-            >
-              <option value="">선택하세요</option>
-              <option value="male">남성</option>
-              <option value="female">여성</option>
-            </select>
-          </div>
-          <div>
-            <label>
-              <input
-                type="checkbox"
-                id="admin-check"
-                name="admin"
-                onChange={handleAdminCheckbox}
-                style={{ marginTop: "40px" }}
-              />
-              사장님(판매자)
-            </label>
-            {isAdmin && (
-              <input
-                type="password"
-                id="admin-token"
-                name="adminToken"
-                placeholder="사장님 키"
-                className={styles["signup-input-box"]}
-              />
-            )}
-          </div>
-          <button className={styles["signup-id-submit"]} onClick={handleSignup}>
-            회원가입
-          </button>
-          <p>{message}</p>
+      <Header /> {/* Render the Header component here */}
+      <div className={styles["signup-form"]}>
+        <div className={styles["signup-title"]}>회원가입</div>
+        <div className={styles["signup-input-container"]}>
+          <div className="signup-id-label">이메일</div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className={styles["signup-input-box"]}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={handleEmailBlur} // Call email validation on blur
+          />
         </div>
+        <div className={styles["signup-input-container"]}>
+          <div className="signup-id-label">비밀번호</div>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className={styles["signup-input-box"]}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className={styles["signup-input-container"]}>
+          <div className="signup-id-label">비밀번호확인</div>
+          <input
+            type="password"
+            name="password_confirm"
+            id="password_confirm"
+            className={styles["signup-input-box"]}
+            value={password_confirm}
+            onBlur={() => {
+              if (password !== password_confirm) {
+                setPasswordMismatch(true);
+              } else {
+                setPasswordMismatch(false);
+              }
+            }}
+            onChange={(e) => {
+              setPasswordConfirm(e.target.value);
+            }}
+          />
+          {passwordMismatch && (
+            <p style={{ color: "red" }}>
+              비밀번호와 비밀번호 확인의 내용이 다릅니다.
+            </p>
+          )}
+        </div>
+        <div className={styles["signup-input-container"]}>
+          <div className="signup-id-label">닉네임</div>
+          <input
+            type="text"
+            name="nickname"
+            id="nickname"
+            className={styles["signup-input-box"]}
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+        </div>
+        <div className={styles["signup-input-container"]}>
+          <div className="signup-id-label">도시</div>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            className={styles["signup-input-box"]}
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+        <div className={styles["signup-input-container"]}>
+          <div className="signup-id-label">성별</div>
+          <select
+            name="gender"
+            id="gender"
+            className={styles["signup-input-box"]}
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value="">선택하세요</option>
+            <option value="male">남성</option>
+            <option value="female">여성</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              id="admin-check"
+              name="admin"
+              onChange={handleAdminCheckbox}
+              style={{ marginTop: "40px" }}
+            />
+            사장님(판매자)
+          </label>
+          {isAdmin && (
+            <input
+              type="password"
+              id="admin-token"
+              name="adminToken"
+              placeholder="사장님 키"
+              className={styles["signup-input-box"]}
+            />
+          )}
+        </div>
+        <button className={styles["signup-id-submit"]} onClick={handleSignup}>
+          회원가입
+        </button>
+        <p>{message}</p>
       </div>
     </div>
   );
