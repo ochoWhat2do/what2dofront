497b219a7a018fe67e28e0643b97c8e518e99175
choisinhye <tls9607@naver.com>
2023-09-06 17:59:50 +0900

    마이페이지에서 나의 리뷰에서 1개의 리뷰 클릭 시 해당 리뷰 상세 페이지로 이동, 나의 가게에서 1개의 가게 클릭 시 해당 가게 상세 페이지로 이동.

--- src/pages/users/myPage.tsx
+++ src/pages/users/myPage.tsx
@@ -25,6 +25,9 @@ interface Review {
   createdAt: Date
   likeCount: number
   attachment: S3FileDto[]
+  createEmail: string
+  storeId: number
+  rate: number
 }
 
 interface S3FileDto {
@@ -40,8 +43,6 @@ const MyPage = () => {
   const [password, setPassword] = useState('')
   const [message, setMessage] = useState('')
   const { storeKey } = router.query // 쿼리 파라미터 가져오기
-  const [storeFavorite, setStoreFavorite] = useState([])
-  const [userReviews, setUserReviews] = useState([])
   const { storeId, reviewId } = router.query // 쿼리 파라미터 가져오기
   //backend 주소
   const indexHost = 'http://localhost:8080' // 로컬
@@ -51,6 +52,7 @@ const MyPage = () => {
   const [storeList, setStoreList] = useState<Store[]>([])
   const [storeStoreKey, setStoreKey] = useState('')
   const [reviewList, setReviewList] = useState<Review[]>([])
+  const [myStores, setMyStores] = useState([])
 
   const user_info = getCookie('user_info')
 
@@ -77,6 +79,10 @@ const MyPage = () => {
         },
       )
       console.log(response.data)
+      const reviewList = response.data
+
+      // Update the state with the reviewElements
+      setReviewList(reviewList)
     } catch (error) {
       console.error('Error fetching 리뷰:', error)
     }
@@ -94,72 +100,96 @@ const MyPage = () => {
         },
       })
       console.log(response.data)
+      setStoreList(response.data.storeFavoriteList) // Set the fetched data to the state
     } catch (error) {
       console.error('Error fetching 가게 목록:', error)
     }
   }
 
-  // useEffect(() => {
-  //   // Check if storeModel has a value before calling getReviews
-  //   if (reviewModel) {
-  //     getComments()
-  //   }
-  // }, [reviewModel])
+  const handleReviewClick = (
+    e: React.MouseEvent,
+    reviewId: number,
+    storeId: number,
+  ) => {
+    e.preventDefault() // 기본 이벤트(링크 이동) 방지
+    const reviewIdStr = reviewId.toString()
+    const storeIdStr = storeId.toString()
+    // 페이지 이동 및 데이터 전달
+    router.push({
+      pathname: '/review/reviewDetail',
+      query: { storeId: storeIdStr, reviewId: reviewIdStr },
+    })
+  }
 
-  //   // Axios 또는 다른 방법을 사용하여 사용자 리뷰 데이터 가져오기
-  //   const response = await axios
-  //     .getReiviews('${devHost}/api/users/{userId}/reviews', {
-  //       headers: {
-  //         Authorization: bearer + auth,
-  //       },
-  //     })
-  //     .then((response) => {
-  //       setUserReviews(response.data)
-  //     })
-  //     .catch((error) => {
-  //       console.error('나의 리뷰 가져오기 실패:', error)
-  //     })
-  // }, [])
-
-  //     const handleMyPage = async () => {
-  //       try {
-  //         const response = await axios.post(devHost + '/api/users/myPage', {
-  //           email,
-  //           password,
-  //         })
-
-  //         if (response.status === 200) {
-  //           window.alert('로그인에 성공하였습니다.')
-  //           // Save tokens in cookies
-  //           setCookie('authorization', response.headers['authorization'], {
-  //             path: '/',
-  //             secure: true,
-  //             sameSite: 'none',
-  //           })
-  //           setCookie(
-  //             'authorization_refresh',
-  //             response.headers['authorization_refresh'],
-  //             {
-  //               path: '/',
-  //               secure: true,
-  //               sameSite: 'none',
-  //             },
-  //           )
-
-  //           router.push('/')
-  //         }
-  //       } catch (error: any) {
-  //         window.alert('로그인에 실패하였습니다. 다시 시도해주세요.')
-  //       }
-  //     }
+  const handleStoreClick = (e: React.MouseEvent, storeKey: string) => {
+    e.preventDefault() // 기본 이벤트(링크 이동) 방지
+    const store = storeKey.toString()
+    // 페이지 이동 및 데이터 전달
+    router.push({
+      pathname: '/store/storeView',
+      query: { storeKey: store },
+    })
+  }
 
   return (
     <div>
       <Header />
       <div className={styles['MyPage-form']}>
         {' '}
-        <h2>마이페이지</h2>
-        <div className=""></div>
+        <h2 className={styles.h2}>마이페이지</h2>
+        <div className={styles.container}>
+          <div className={styles.nameBox}>
+            <h1>나의 리뷰</h1>
+          </div>
+          <ul>
+            <div className={styles.reviewContainer}>
+              {reviewList.map((review) => (
+                <div
+                  key={review.id}
+                  className={styles.reviewItem}
+                  onClick={(e) =>
+                    handleReviewClick(e, review.id, review.storeId)
+                  }
+                  style={{ cursor: 'pointer' }}
+                >
+                  <h3 className={styles.reviewTitle}>{review.title}</h3>
+                  <p className={`${styles.reviewRate} ${styles.rate}`}>
+                    <span className={styles.rate}>별점:</span> {review.rate}
+                  </p>
+                  <p className={styles.reviewContent}>{review.content}</p>
+                  <p className={styles.reviewDate}>
+                    Created At: {new Date(review.createdAt).toLocaleString()}
+                  </p>
+                  <p className={styles.reviewLikes}>
+                    Likes: {review.likeCount}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </ul>
+          <div className={styles.nameBox2}>
+            <h1>나의 가게 목록</h1>
+          </div>
+          <ul>
+            <div className={styles.storeContainer}>
+              {storeList.map((store) => (
+                <div
+                  key={store.id}
+                  className={styles.storeItem}
+                  onClick={(e) => handleStoreClick(e, store.storeKey)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <h3 className={styles.storeTitle}>{store.title}</h3>
+                  <p className={styles.storeCategory}>{store.category}</p>
+                  <p className={styles.storeAddress}>{store.address}</p>
+                  <p className={styles.storeHomePageLink}>
+                    {store.homePageLink}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </ul>
+        </div>
       </div>
     </div>
   )
