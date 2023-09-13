9f2cb0aa5182f6002f18d29dddd7e73dbb634234
aiva <42510512+sxi8022@users.noreply.github.com>
2023-09-13 09:16:15 +0900

    [fix] 로그아웃 안되는현상 수정 (#40)
    * [fix] 메인페이지 검색 방식 변경
    
    * 컴파일에러수정
    
    * [fix] 소셜로그인 시 쿠키 삭제 안되는현상 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,7 +87,7 @@ export default function Home() {
       return
     }
 
-    // 검색어에 "맛집" 또는 "까페"가 포함되어 있는지 확인
+    // 검색어에 "맛집" 포함여부 확인
     if (!words.some((word) => ['맛집'].includes(word))) {
       window.alert("검색어에 '맛집'을 포함해야 합니다.")
       return
--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -11,5 +11,5 @@ export const getCookie = (name: string) => {
 }
 
 export const removeCookie = (name: string) => {
-  return cookies.remove(name)
+  return cookies.remove(name, { path: '/', maxAge: 0 }) // maxAge를 0으로 설정하여 지속 시간을 0으로 만듭니다.
 }
